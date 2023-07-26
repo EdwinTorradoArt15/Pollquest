bb03148e08dd8f3f86a61bcdfec70f877dbd84cc
EdwinTorradoArt15 <torradoart15@gmail.com>
2023-07-26 17:01:03 -0500

    Cambios menores

--- apps/client/src/features/cuestionarios/pages/Cuestionarios.tsx
+++ apps/client/src/features/cuestionarios/pages/Cuestionarios.tsx
@@ -1,12 +1,34 @@
 import { Header } from "@/components";
-import { Stack } from "@mui/material";
+import { Stack, useMediaQuery, Button, SvgIcon, Theme } from "@mui/material";
+import { FiPlus } from "react-icons/fi";
 
 const Cuestionarios = () => {
+  const smUp = useMediaQuery((theme: Theme) => theme.breakpoints.up("sm"), {
+    defaultMatches: true,
+    noSsr: false,
+  });
   return (
     <>
       <Stack spacing={3}>
-        <Stack spacing={1}>
-          <Header title="Cuestionarios" subtitle="Mis cuestionarios." />
+        <Stack direction="row" justifyContent="space-between" spacing={4}>
+          <Stack spacing={1}>
+            <Header title="Cuestionarios" subtitle="Crea tus cuestionario." />
+          </Stack>
+          <div>
+            <Button
+              sx={{
+                fontSize: smUp ? "1rem" : "0.75rem",
+              }}
+              startIcon={
+                <SvgIcon fontSize="small">
+                  <FiPlus />
+                </SvgIcon>
+              }
+              variant="contained"
+            >
+              Crear
+            </Button>
+          </div>
         </Stack>
       </Stack>
     </>
